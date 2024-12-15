cb5de27517fd6a8ba6ba33b88c9da311481f1259
Furkan Kuyumcu <fkuyumcu@k1m23s04.42istanbul.com.tr>
2024-12-15 16:05:08 +0300

    ilk

--- pipex.c
+++ pipex.c
@@ -1,30 +1,103 @@
-void pipex(int file1, int file2, char *cmd1, char *cmd2)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 11:13:47 by fkuyumcu          #+#    #+#             */
+/*   Updated: 2024/12/15 15:36:28 by fkuyumcu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char *find(char *cmd, char **envp)
 {
-	int	end[2];
-	int	f;
-	pipe(end);
-	f = fork();
-	if (parent == 0)
+	
+}
+
+void liberation(char **)
+
+void	execute(char *argv, char **envp)
+{
+	char **command;
+	int i;
+	char *path;
+
+	i = -1;
+	command = ft_split(argv, ' ');
+	path = find(command[0],envp);
+	if (path == 0)//burayı incele/değiştirmen gerekebilir
 	{
-		child(file1,cmd2);
+		while (command[++i])
+			free(command[i]);
+		free(command);
+		error();
 	}
-	else if (parent > 0)
+	if (execve(path,argv,envp) == -1)
+		error;
+}
+
+
+void child(char **argv,char **envp, int *fd)
+{
+	int	infile;
+	infile = open(argv[1],O_RDONLY,0777);
+	if(infile < 0)
+		error();
+		
+	dup2(infile,STDIN_FILENO);
+	dup2(fd[1],STDOUT_FILENO);
+	close(fd[0]); //pipe'ın fd[0] ucunu kullanmayacağımdan dolayı kapatıyorum.
+	execute(argv[2],envp);
+
+}
+
+void parent(char **argv, char **envp, int *fd)
+{
+	int outfile;
+	outfile = open(argv[4], O_RDONLY, 0777);
+	
+	if(outfile < 0)
+		error();
+	dup2(outfile, STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[1]);//2.prosesten 1.prosese veri aktarımı olmayacağından dolayı pipe'ın parent ucunu kapatıyorum.
+	execute(argv[3],envp);
+}
+
+
+
+void error(void)
+{
+	perror("An error occured.");
+	exit(EXIT_FAILURE);
+}
+
+
+void main(int argc, char **argv, char **envp)
+{
+	int fd[2];
+	pid_t pid;
+	
+	if (argc == 5)
 	{
-		parent();
-	}
+		if(pipe(fd) == -1)
+			return ("error");
+		pid = fork();
+		if(pid == 0)
+		{	
+			child(argv,envp,fd);	
+		}
+		waitpid(pid,NULL,0);
+		parent(argv,envp,fd);
+	}	
+		
 	else
-		return (perror("An Error Occured.\n")); //error
-
-
-}
-
-
-void child(int f1,char *cmd1)
-{
-
-}
-
-void parent(int f2, char *cmd2)
-{
+	{
+		ft_printf("Error: Bad Arguments\n");
+		ft_printf("E.g. <file1> <cmd1> <cmd2> <file2> \n");
+	}
 
 }
---
+++ pipex.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 13:59:48 by fkuyumcu          #+#    #+#             */
+/*   Updated: 2024/12/15 15:09:18 by fkuyumcu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+
+/* Mandatory functions */
+void	error(void);
+char	*find(char *cmd, char **envp);
+int		get_next_line(char **line);
+void	execute(char *argv, char **envp);
+
+/* Bonus functions */
+int		open_file(char *argv, int i);
+void	usage(void);
+
+#endif
